d21b2aaebae42be274626b41950b908a67a2fbed
Johannes <Johannes.nilson@outlook.com>
2026-03-19 21:58:36 +0100

    initial commit

---
+++ src/parser.py
@@ -0,0 +1,11 @@
+
+
+def parse_md(text:str)->str:
+    '''
+    Takes a string comprising a .md document,
+    and returns a string containing the equivalent
+    HTML. 
+    
+    '''
+
+    return text
