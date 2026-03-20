2789e3747ed247b40d060ae4f16a296c2658842d
Johannes <Johannes.nilson@outlook.com>
2026-03-20 09:56:13 +0100

    added test

---
+++ test_.py
@@ -0,0 +1,36 @@
+
+# fix importing issues
+import sys
+from pathlib import Path
+directory = str(Path(__file__).parent.resolve()) # the main directory
+sys.path.append(directory)
+
+from src.parser import parse_md
+import json
+import pytest
+
+
+def load_data()->list[dict]:
+    with open("test-data/spec.json", 'r', encoding="utf-8") as file:
+
+        lines = file.readlines()
+        spec = json.loads("".join(lines))
+        # spec = json.load(file, encoding="utf-8", errors="ignore")
+    return list(spec)
+
+
+spec = load_data()
+
+@pytest.mark.parametrize('specs', spec)
+def test_check(specs):
+
+    md:str = specs['markdown']
+    html:str = specs['html']
+    guess = parse_md(md)
+    assert guess.strip() == html.strip()
+
+
+
+if __name__ == "__main__":
+
+    load_data()
